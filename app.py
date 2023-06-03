c674f82ee176e3a25607a10dc8e1753e7166bf4e
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2023-06-03 08:34:24 +0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 image_path = 'https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png'
 st.image(image_path)
-st.title('Hi there; This app for EDA staff')
+st.title('Hi there; this app for EDA staff')
 # Display a message
 st.write('This is a simple app for calculating square of numbers')
 x = st.slider('Select a value')
