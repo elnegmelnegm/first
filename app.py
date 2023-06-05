0093df4886613f56d28414f2b32f13ba6224b8c6
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2023-06-05 04:30:28 +0300

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ with col1:
   st.subheader("Standard")
   standard_values_st = []
   for i in range(5):
-    standard_values_st.append(float(st.number_input("Area of standard", key="standard_value_{}".format(i), default="")))
+    standard_values_st.append(float(st.number_input("Area of standard", key="standard_value_{}".format(i), default="0")))
   average = round(np.mean(standard_values_st),2)
   standard_deviation = round(np.std(standard_values_st),2)
   RSD = round(standard_deviation*100/average,2)
@@ -26,16 +26,16 @@ with col2:
   st.subheader("Sample")
   standard_values_s = []
   for i in range(3):
-    standard_values_s.append(float(st.number_input("Area of sample", key="standard_value_sample_{}".format(i), default="")))
+    standard_values_s.append(float(st.number_input("Area of sample", key="standard_value_sample_{}".format(i), default="0")))
   average_s = round(np.mean(standard_values_s),2)
   st.write("Average of sample:", average_s)
 
 ########sample
 with st.container():
   st.subheader("The percentage of assay")
-  standard_concentration = float(st.number_input("Concentration of standard (µg/mL)", key="standard_concentration", default=""))
-  sample_concentration = float(st.number_input("Concentration of sample (µg/mL)", key="sample_concentration", default=""))
-  standard_purity = float(st.number_input("Purity of standard (%)", key="purity", default=""))
+  standard_concentration = float(st.number_input("Concentration of standard (µg/mL)", key="standard_concentration", default="0"))
+  sample_concentration = float(st.number_input("Concentration of sample (µg/mL)", key="sample_concentration", default="0"))
+  standard_purity = float(st.number_input("Purity of standard (%)", key="purity", default="0"))
   assay = average_sstandard_puritystandard_concentration/(average*sample_concentration)
   assay2 = round(assay,2)
   st.write("Assay:", assay2, "% ")
